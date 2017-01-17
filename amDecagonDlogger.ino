57ecfd4ccdb623cec19c655785c156c2f26bf76d
DESKTOP-GRJF5G2\klock <neto.nemesis@gmail.com>
2017-01-17 13:32:57 -0200

    Versão 17/01/2017 Correções para o RTC Correções para o Watchdog (agora funciona e reseta em caso de falha e as 23h) Melhoras no código

--- amDecagonDlogger.ino
+++ amDecagonDlogger.ino
@@ -1,30 +1,40 @@
 #include "amDecagonDlogger.h"
 #define Reset_AVR() wdt_enable(WDTO_30MS); while(1) {}
 RTC_DS1307 rtc;
+
+void checkHour(){
+   DateTime d = rtc.now();
+   uint8_t h=d.hour();
+   if(h==23){
+    Serial.println("resetando..");
+    Reset_AVR();
+    }
+  }
 void initSensor() {
   SDISerial connection(9, 1);
   connection.begin();
   Serial.println("Iniciando Sistema"); // startup string echo'd to our uart
   delay(3000);
-  Serial.println(connection.sdi_query("1I!", 1000));
+  Serial.println(connection.sdi_query("0I!", 1000));
 }
 void getSensorData() {
+  checkHour();
   SDISerial connection(9, 1);
   char output_buffer[255], nomearquivo[20], dados[1][20];
   connection.begin();
-  DateTime now = rtc.now();
-  connection.sdi_query("1M!", 1000);
+  DateTime d = rtc.now();
+  connection.sdi_query("0M!", 1000);
   connection.wait_for_response(1000);
-  char* resposta = connection.sdi_query("1D0!", 1000);
+  char* resposta = connection.sdi_query("0D0!", 1000);
   strcpy(dados[0], resposta);
   Serial.print("Sensor 0:");
   Serial.println(resposta);
-  sprintf(nomearquivo, "%02d%02d%02d.CSV", now.day(), now.month(), now.hour());
+  sprintf(nomearquivo, "%02d%02d%02d.CSV", d.day(), d.month(), d.hour());
   File arquivo = SD.open(nomearquivo, FILE_WRITE);
   if (!arquivo) {
     Serial.print("Problema no Arquivo");
     Serial.println(nomearquivo);
-    while (1);
+    Reset_AVR()
   }
   else {
     for (uint8_t x = 0; x < 1; x++) {
@@ -35,35 +45,39 @@ void getSensorData() {
         if (dados[x][y] == '\r') dados[x][y] = dados[x][y + 1];
       }
     }
-    sprintf(output_buffer,"%02d/%02d/%i;%02d:%02d:%02d;%s;\r\n",now.day(),now.month(),now.year(),now.hour(),now.minute(),now.second(),dados[0]);
+    sprintf(output_buffer,"%02d/%02d/%i;%02d:%02d:%02d;%s;\r\n",d.day(),d.month(),d.year(),d.hour(),d.minute(),d.second(),dados[0]);
     Serial.println("dados cartão!");
     Serial.println(output_buffer);
     arquivo.print(output_buffer);
     arquivo.close();
-    wdt_reset();
   }
- 
-
 }
 
 void setup() {
+  
   Serial.begin(115200); // start our uart
+//  while (!Serial) {
+//    ; // wait for serial port to connect. Needed for native USB port only
+//  }
+  #ifdef AVR
+  Wire.begin();
+  #else
+  Wire1.begin(); // Shield I2C pins connect to alt I2C bus on Arduino Due
+  #endif
+  rtc.begin();
+  if (! rtc.isrunning()) {
+    Serial.println("RTC is NOT running!");
+    rtc.adjust(DateTime(__DATE__, __TIME__));
+  }
   Serial.print("Testando Cartao...");
   pinMode(10, OUTPUT);
   if (!SD.begin(10)) {
     Serial.println("Falha no Cartao..");
     return;
   }
-  Wire.begin();
-  if (! rtc.isrunning()) {
-    Serial.println("Testando RTC");
-    // following line sets the RTC to the date & time this sketch was compiled
-    rtc.adjust(DateTime(__DATE__, __TIME__));
-  }
   initSensor();
 }
 void loop() {
-
   getSensorData();
   delay(1000);
 }
